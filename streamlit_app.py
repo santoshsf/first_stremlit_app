49295d15fa2bd3b5276b282d360b1fad0230f152
santoshsf <124996795+santoshsf@users.noreply.github.com>
2023-02-10 13:36:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('New Healthy Diner')
-streamlit.Header('Menu')
+streamlit.header('Menu')
 streamlit.text('item-1')
 streamlit.text('item-2')
 streamlit.text('item-3')
