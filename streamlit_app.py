a9265c45bfd1b20c252517f987f9422179d12a45
santoshsf <124996795+santoshsf@users.noreply.github.com>
2023-02-10 14:23:20 +0530

    Update streamlit_app.py
    Used python package to read the external file

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,8 @@ streamlit.text('item-1')
 streamlit.text('item-2')
 streamlit.text('item-3')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+import Pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
